7e1abb04976666c263ecf9c8661e8d8b62fea6d2
naornaoj <naornaoj@gmail.com>
2024-06-29 22:11:23 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -88,6 +89,8 @@ Route::get('/thyroid-results', function () {
 //  Route::get('/dashboard', function () {
 //      return view('dashboard');
 //  })->name('dashboard');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
