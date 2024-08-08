f4e5e8c3405efbf62722a89a3bb1ee4235b8d4a9
naornaoj <naornaoj@gmail.com>
2024-08-08 21:44:42 +0800

    modified:   resources/views/layouts/features.blade.php 	modified:   resources/views/ua-results.blade.php 	modified:   routes/auth.php

--- resources/views/layouts/features.blade.php
+++ resources/views/layouts/features.blade.php
@@ -1,15 +1,15 @@
  <!-- Features Start -->
  <div class="container-fluid py-5">
           <div class="container">
-              <div class="row g-0 feature-row">
+              <div class="row g-0 feature-row d-flex">
                   <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.1s">
                     <a href="{{ url('express-diagnostics') }}">
                         <div class="feature-item border h-100 p-5">
-                            <div class="icon-box-primary mb-4">
-                                <i class="bi bi-heart-pulse"></i>
+                            <div class="icon-box-primary mb-4 ">
+                                <i class="bi bi-heart-pulse "></i>
                             </div>
                             <h5 class="mb-3 mt-3">Express Diagnostics</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
+                            <!-- <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p> -->
                         </div>
                     </a>
                   </div>
@@ -20,7 +20,7 @@
                                 <i class="bi bi-clipboard-pulse"></i>
                             </div>
                             <h5 class="mb-3">Express Lab Results</h5>
-                            <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
+                            <!-- <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p> -->
                         </div>
                     </a>
                   </div>
@@ -31,7 +31,7 @@
                                 <i class="bi bi-building-fill-gear text-primary"></i>
                             </div>
                             <h5 class="mb-3">Company Services</h5>
-                            <p class="mb-0 text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
+                            <!-- <p class="mb-0 text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p> -->
                         </div>
                     </a>
                   </div>
@@ -42,7 +42,7 @@
                                  <i class="bi bi-house-gear-fill text-primary"></i><i class="bi bi-virus2"></i>
                             </div>
                             <h5 class="mb-3">Home Service</h5>
-                            <p class="mb-0 text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
+                            <!-- <p class="mb-0 text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p> -->
                         </div>
                     </a>
                   </div>
--- resources/views/ua-results.blade.php
+++ resources/views/ua-results.blade.php
@@ -4,36 +4,48 @@
             @csrf
             
             <h1 class="text-center font-bold text-xl mt-4">Patient Details</h1>
-            <div class="col-md-4">
-                <label for="fname" class="form-label">First Name</label>
+            <div class="col-md-6">
+                <label for="fname" class="form-label">PATIENT NAME</label>
                 <input type="text" class="form-control" id="fname" required>
                 <div class="valid-feedback">
                 Looks good!
                 </div>
             </div>
-            <div class="col-md-4">
-                <label for="mname" class="form-label">Middle Name</label>
-                <input type="text" class="form-control" id="mname">
+            <div class="col-md-3">
+                <label for="requestNo" class="form-label">REQUEST NO</label>
+                <input type="text" class="form-control" id="requestNo">
             </div>
-            <div class="col-md-4">
-                <label for="lname" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lname">
+            <div class="col-md-3">
+                <label for="orNo" class="form-label">OR NO</label>
+                <input type="number" class="form-control" id="orNo">
             </div>
-            <div class="col-md-6">
-                <label for="age" class="form-label">Age</label>
+            <div class="col-md-2">
+                <label for="age" class="form-label">AGE</label>
                 <input type="number" class="form-control" id="age">
             </div>
-            <div class="col-md-6">
-                <label for="gender" class="form-label">Gender</label>
+            <div class="col-md-2">
+                <label for="gender" class="form-label">GENDER</label>
                 <input type="text" class="form-control" id="gender">
             </div>
-            <div class="col-md-6">
-                <label for="address" class="form-label">Address</label>
+            <div class="col-md-8">
+                <label for="address" class="form-label">ADDRESS</label>
                 <input type="text" class="form-control" id="address" placeholder="1234 Main St">
             </div>
-            <div class="col-md-6">
-                <label for="orNum" class="form-label">Official Receipts</label>
-                <input type="number" class="form-control" id="orNum">
+            <div class="col-md-3">
+                <label for="rDoctor" class="form-label">REQUESTING DOCTOR</label>
+                <input type="text" class="form-control" id="rDoctor">
+            </div>
+            <div class="col-md-3">
+                <label for="dateTimeReq" class="form-label">DATE & TIME REQUESTED</label>
+                <input type="date" class="form-control" id="dateTimeReq">
+            </div>
+            <div class="col-md-3">
+                <label for="dateTimePerf" class="form-label">DATE & TIME PERFORMED</label>
+                <input type="date" class="form-control" id="dateTimePerf">
+            </div>
+            <div class="col-md-3">
+                <label for="dateTimeVal" class="form-label">DATE & TIME VALIDATED</label>
+                <input type="date" class="form-control" id="dateTimeVal">
             </div>
             <h1 class="text-center font-bold text-xl mt-4" >Urinalysis</h1>
 
@@ -111,7 +123,7 @@
                     </tbody>
                 </table>
             </div>
-
+            @include('client-pages.licence-footer') 
             <div class="col-12">
                 <button type="submit" class="btn btn-success float-end my-4">Print Results</button>
             </div>
