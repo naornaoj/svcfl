6a589e781be9b674e69de2681181142785f5b82c
naornaoj <naornaoj@gmail.com>
2024-08-08 22:06:57 +0800

    modified:   resources/views/layouts/service.blade.php

--- resources/views/layouts/service.blade.php
+++ resources/views/layouts/service.blade.php
@@ -29,11 +29,11 @@
                   <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                       <div class="service-item">
                           <div class="icon-box-primary mb-4">
-                              <i class="bi bi-virus text-dark"></i>
+                            <i class="bi bi-prescription text-dark"></i>
                           </div>
                           <h5 class="mb-3">Drug Testing</h4>
                               <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
-                          <a class="btn btn-light px-3" href="">Read More<i class="bi bi-chevron-double-right ms-1"></i></a>
+                          <a class="btn btn-light px-3" href="">Read More<i class="bi bi-prescription"></i></a>
                       </div>
                   </div>
                   <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
@@ -49,7 +49,7 @@
                   <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                       <div class="service-item">
                           <div class="icon-box-primary mb-4">
-                              <i class="bi bi-capsule text-dark"></i>
+                            <i class="bi bi-file-earmark-medical-fill text-dark"></i>
                           </div>
                           <h5 class="mb-3">Medical Consultations</h4>
                               <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
@@ -69,7 +69,7 @@
                   <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                       <div class="service-item">
                           <div class="icon-box-primary mb-4">
-                              <i class="bi bi-clipboard2-pulse text-dark"></i>
+                            <i class="bi bi-eyedropper text-dark"></i>
                           </div>
                           <h5 class="mb-3">DNA Testing</h4>
                               <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
@@ -79,7 +79,7 @@
                   <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
                       <div class="service-item">
                           <div class="icon-box-primary mb-4">
-                              <i class="bi bi-file-medical text-dark"></i>
+                            <i class="bi bi-virus text-dark"></i></i>
                           </div>
                           <h5 class="mb-3">Covid Testing</h4>
                               <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tellus augue.</p>
