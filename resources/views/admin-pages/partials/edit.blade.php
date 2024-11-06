4b461314c3b9e86316511119e6bfa7db164fcc60
naornaoj <naornaoj@gmail.com>
2024-11-06 21:42:12 +0800

    modified:   resources/views/admin-pages/partials/edit.blade.php

--- resources/views/admin-pages/partials/edit.blade.php
+++ resources/views/admin-pages/partials/edit.blade.php
@@ -30,7 +30,7 @@
                         <x-input-error class="mt-2" :messages="$errors->get('lname')" />
                     </div>
                     <div class="col-span-6 sm:col-span-3">
-                        <x-input-label for="role" :value="__('Last Name')" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" />
+                        <x-input-label for="role" :value="__('Role')" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" />
                         <x-text-input id="role" name="role" type="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="Role" />
                         <x-input-error class="mt-2" :messages="$errors->get('role')" />
                     </div>
