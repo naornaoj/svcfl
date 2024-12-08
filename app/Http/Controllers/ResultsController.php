8fb3bdb079bc5056da490ecd7fbf54eaecf4fc88
naornaoj <naornaoj@gmail.com>
2024-12-08 16:03:12 +0800

    modified:   app/Http/Controllers/ResultsController.php 	modified:   resources/views/admin-pages/diagnostics/bloodtype.blade.php

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -29,8 +29,6 @@ class ResultsController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //$results = $request->input('selectedIndividualId');
-        // $selectedIndividualId = IndividualTest::first()->IndividualId;
         $usermail = $request->email;
 
         $results = Results::create([
@@ -152,50 +150,6 @@ class ResultsController extends Controller
         return redirect(route('results', absolute: false))->with('success', 'Results Sucessfully Saved!');
     }
 
-    // public function packagesList()
-    // {
-    
-    //     $packages = DB::table('packages')->select('id', 'packageName')->get();
-    //     $individualTest = DB::table('individualtest')->select('id', 'individualTest')->get();
-
-    //     return view('express-diagnostics', [
-    //         'packages'=>$packages, 'individualTest'=>$individualTest
-    //     ]);
-
-    // }
-
-    // public function individualTestList()
-    // {
-    //     $individualTest = DB::table('individualtest')->select('id', 'individualTest')->get();
-
-    //     return view('express-diagnostics', [
-    //         'individualTest'=>$individualTest
-    //     ]);
-
-    // }
-
-    /**
-     * Display the packages list.
-     */
-    public function search(Request $request): Response
-    {
-        if(!empty($request->id))
-        {
-            $id = $request->id;
-            $patientInfo = DB::select('select * from diagnostics where id = ? limit 1', [$request->id]);
-            return response($patientInfo);
-        }
-
-        if(!empty($request->name))
-        {
-            $id = $request->id;
-            $patientInfo = DB::select('select * from diagnostics where name = ? limit 1', [$request->name]);
-            return response($patientInfo);
-        }
-
-        return response(''); // Return a response
-  
-    }
 
      /**
      * Display the specified resource.
